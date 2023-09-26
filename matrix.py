f25e325f392f898170e446adea0b8713bf9e7d2d
Keerthana Dinesh A <98273850+Keerthana09022001@users.noreply.github.com>
2023-09-26 23:43:10 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -3,18 +3,18 @@ import re
 
 first_multiple_input = input().rstrip().split()
 
-n = int(first_multiple_input[0])
+x = int(first_multiple_input[0])
 
-m = int(first_multiple_input[1])
+y = int(first_multiple_input[1])
 
 matrix = []
 
-for _ in range(n):
+for _ in range(x):
     matrix_item = input()
     matrix.append(matrix_item)
 elements = []
 result=''
-for t in range(m):
+for t in range(y):
     for w in matrix:
         elements.append(w[t])
 result = "".join(elements)
